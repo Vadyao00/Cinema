f27beba0bec0240cfcf89027fd2e296790269ff6
Vadim <vadim.ivashko.04@gmail.com>
2024-10-17 19:31:42 +0300

    Добавлен класс расширений для сервисов, а также внесены небольшие изменения в главном классе программы

---
+++ Cinema.Web/Extensions/ServiceExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Cinema.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cinema.Web.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services) =>
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                builder.AllowAnyHeader().
+                AllowAnyOrigin().
+                AllowAnyMethod());
+            });
+
+        public static void ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration) =>
+            services.AddDbContext<CinemaContext>(opts =>
+                opts.UseSqlServer(configuration.GetConnectionString("DefaultConnectionLocal"), b =>
+                {
+                    b.EnableRetryOnFailure();
+                })
+            );
+
+        //public static void ConfigureRepositoryManager(this IServiceCollection services) =>
+        //    services.AddScoped<IrepositoryManager, RepositoryManager>();
+    }
+}
--- Cinema.Web/Program.cs
+++ Cinema.Web/Program.cs
@@ -1,4 +1,5 @@
 using Cinema.Web.Data;
+using Cinema.Web.Extensions;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,43 +11,49 @@ namespace Cinema.Web
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-            builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(connectionString));
-            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
-            builder.Services.AddControllersWithViews();
+            ConfigureServices(builder.Services,builder.Configuration);
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.UseMigrationsEndPoint();
+                //app.UseMigrationsEndPoint();
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            app.Map("/", () => "Course work");
+            app.Run();
+        }
 
+        public static void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.ConfigureCors();
+
+            services.ConfigureSqlContext(configuration);
+
+
+
+        }
+
+        public static void ConfigureApp(IApplicationBuilder app)
+        {
             app.UseHttpsRedirection();
+
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = Microsoft.AspNetCore.HttpOverrides.ForwardedHeaders.All
+            });
+            app.UseHttpsRedirection();
+            app.UseCors("CorsPolicy");
+
             app.UseStaticFiles();
 
             app.UseRouting();
 
             app.UseAuthorization();
 
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
-            app.MapRazorPages();
-
-            app.Run();
         }
     }
 }
